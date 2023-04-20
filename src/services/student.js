1b837f35f29b95c81638457c3cd9a2cbbe8b124f
Nawaz-swivel <nawas@swivelgroup.com.au>
2023-04-20 22:28:06 +0530

    changed urls

--- src/services/student.js
+++ src/services/student.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const url = process.env.REACT_APP_STUDENT_API_URL;
-const url = 'http://18.204.14.92:8080/ignite-student-service';
+const url = 'http://54.237.225.107:8080/ignite-student-service';
 
 export const STUDENT_API = (token) => {
     // const API = axios.create({baseURL: 'http://localhost:8080/ignite-student-service'});
